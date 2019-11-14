cbb273afaa3f66d1bb3d5da742dd95f2078615e9
luciocaridi <56658801+luciocaridi@users.noreply.github.com>
2019-11-13 21:52:34 -0300

    Add files via upload

---
+++ Sprint 2/models/Itinerary.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ItinerarySchema = new Schema({
+    title: {
+        type: String
+    },
+    profilePic: {
+        type: String
+    },
+    rating: {
+        type: Number
+    },
+    duration: {
+        type: Number
+    },
+    price: {
+        type: Number
+    },
+    hashtag: {
+        type: Array
+    }
+});
+
+module.exports = Itinerary = mongoose.model("itinerary", ItinerarySchema);
